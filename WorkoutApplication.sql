b1aa0a0c2a1da2a55b71b9f6e9f685911fe02613
Scarlett Crakes <crakes@usc.edu>
2024-12-07 14:34:01 -0800

    Ok now fully updated SQL Table

--- WorkoutApplication.sql
+++ WorkoutApplication.sql
@@ -4,7 +4,8 @@ USE WorkoutApplication;
 
 CREATE TABLE users (
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     profile_image TEXT,
